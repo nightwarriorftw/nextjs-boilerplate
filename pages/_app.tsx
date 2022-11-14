771dffa766cc87bbd7d0818a7c46a0e39f19a069
nightwarriorftw <amanv1999@gmail.com>
2022-11-14 22:31:04 +0530

    nextjs boilerplate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,5 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <h1 className='text-3xl font-bold underline'>Happy Hacking</h1>
 }
